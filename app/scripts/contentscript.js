22a6408373efd59fb5ef336ef0e39719a7596799
Chris Boette <cboette@gmail.com>
2015-01-27 21:13:52 -0600

    Mostly sorta works, shit ass. Mostly meaning, half the time and the params for the image randomizer are the same for every image.

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,18 +2,8 @@ var canvas,
     ctx,
     id;
 
-var addCanvas = function( image, id ){
+var replaceImage = function ( image ) {
   'use strict';
-  
-  // 2. add canvas object to page
-  var imgWidth = image.context.width;
-  var imgHeight = image.context.height;
-
-  $(image).replaceWith('<canvas id="' + id + '" width="' + imgWidth + '" height="' + imgHeight + '"></canvas>');
-
-  // 3. drop img src into canvas *IF JPG/JPEG/jpg/jpeg*
-  canvas = document.getElementById( id );
-  ctx = canvas.getContext('2d');
 
   var cleanImg = new Image();
   cleanImg.src = image.context.src;
@@ -21,32 +11,45 @@ var addCanvas = function( image, id ){
   cleanImg.onload = function () {
     ctx.drawImage( cleanImg, 0, 0, imgWidth, imgHeight );
   };
-
 };
 
-var glitchOut = function( canvas, ctx ){
+var addCanvas = function( image, id ){
+  'use strict';
   
-  // canvas = document.getElementById( id );
-  // ctx = canvas.getContext('2d');
+  var imgWidth = image.context.width;
+  var imgHeight = image.context.height;
+
+  $(image).replaceWith('<canvas id="' + id + '" width="' + imgWidth + '" height="' + imgHeight + '"></canvas>');
+
+  canvas = document.getElementById( id );
+  ctx = canvas.getContext('2d');
+
+  return image;
+};
+
+var generateParams = function () {
+  return { 
+    amount: Math.random() * 90, 
+    seed: Math.random() * 90, 
+    iterations: Math.random() * 90, 
+    quality: Math.random() * 90 
+    // amount: 87, 
+    // seed: 55, 
+    // iterations: 42, 
+    // quality: 69
+  };
+};
+
+var glitchOut = function( canvas, ctx, params ){
+  'use strict';
 
   var my_image_data = ctx.getImageData( 0, 0, canvas.clientWidth, canvas.clientHeight );
 
-  var parameters = { 
-    // amount: Math.random() * 90, 
-    // seed: Math.random() * 90, 
-    // iterations: Math.random() * 90, 
-    // quality: Math.random() * 90 
-    amount: 87, 
-    seed: 55, 
-    iterations: 42, 
-    quality: 69
-  };
-
-  var drawGlitchedImageData = function(image_data) {
+  var makeGlitchedImageData = function(image_data) {
     ctx.putImageData(image_data, 0, 0);
   }
 
-  glitch(my_image_data, parameters, drawGlitchedImageData);
+  glitch(my_image_data, params, makeGlitchedImageData);
 };
 
 // JPG Check - glitch.js only works for jpg/jpeg images
@@ -57,13 +60,14 @@ $(document).on('ready', function(){
     // 1. Create a random ID for the canvas image
     id = (Math.random() * 10000).toFixed().toString();
     var self = $(this);
+    var params = generateParams();
 
     if ((/\.(jpg|jpeg)/i).test( self.context.src) ){
       // drop image into canvas
-      addCanvas( self, id );
-      console.log( canvas );
+      var anImage = addCanvas( self, id );
+      replaceImage( anImage );
       // run the glitching code
-      glitchOut( canvas, ctx );
+      glitchOut( canvas, ctx, params );
     }
   })
 });
